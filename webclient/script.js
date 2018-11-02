e3054b30be0cbb48150cd12f05449d41dd86d30d
ashfaque-rifaye <ashfaque_rifaye@outlook.com>
2018-11-02 07:53:39 +0530

    Added css styling and html for the front end app development project.
    Git Commit-Front End App development (2nd Nov)

---
+++ webclient/script.js
@@ -0,0 +1,13 @@
+// var test = document.getElementByClassName('firstElement');
+// var width = test.offsetWidth - 215;
+
+// test.setAttribute('style', 'background-position:left top, ' + width + 'px top ');
+
+// var data =  "PDA DEI"
+
+function onToggleMenu(){
+	var element =document.getElementsByClassName('collapsable-button');
+	for (var i = 0; i < element.length; i++) {
+		element[i].classList.toggle('active');
+	}
+}
