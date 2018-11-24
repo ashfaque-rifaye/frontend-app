954d6010898033cc73ef1cc841e9c9674596dc13
ashfaque-rifaye <ashfaque_rifaye@outlook.com>
2018-11-25 00:34:21 +0530

    Added effects for CSS styling in Login and Nav-Menus

--- webclient/script.js
+++ webclient/script.js
@@ -1,15 +1,40 @@
-// var test = document.getElementByClassName('firstElement');
-// var width = test.offsetWidth - 215;
 
-// test.setAttribute('style', 'background-position:left top, ' + width + 'px top ');
-
-// var data =  "PDA DEI"
+/* To Toggle the NAV-BAR MENU*/
 
 function onToggleMenu(){
-	var element =document.getElementsByClassName('nav-menu');
-	for (var i = 0; i < element.length; i++) {
-		// element[i].classList.toggle('active');
-		document.getElementsByClassName('navbarList')[0].style.visibility = "visible";
+	if(document.getElementsByClassName('navbarList')[0].style.visibility != null 
+		&& document.getElementsByClassName('navbarList')[0].style.visibility !== "visible")
+			document.getElementsByClassName('navbarList')[0].style.visibility = "visible";
+	else{
+		document.getElementsByClassName('navbarList')[0].style.visibility = "hidden";
 	}
 }
 
+/* To Show or Hide elements on switching the tabs */
+function toggleLoginSignUpTab(event){
+	if(event==='Login'){
+		document.getElementById('login').classList.toggle('active');
+		document.getElementsByClassName('input-text-login').Username.style.display = "none";
+
+	}else{
+		document.getElementById('signUp').classList.toggle('active');
+		document.getElementsByClassName('input-text-login').Username.style.display = "inline";
+	}
+	
+}
+
+
+function openLoginPopup() {	
+
+	if(document.getElementsByClassName('login-form')[0].style.display != null 
+		&& document.getElementsByClassName('login-form')[0].style.display !== "none"){
+			
+		document.getElementsByClassName('login-form')[0].style.display = "block";
+		document.getElementById('login').classList.toggle('active');
+
+	}
+	else{
+		document.getElementsByClassName('login-form')[0].style.display = "none";
+	}
+	
+}
